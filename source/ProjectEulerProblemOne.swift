dca2f394f934f57d1499e634d8edb84ff0137493
Austin Van Alfen <austin.van.alfen@gmail.com>
2019-06-24 21:38:10 -0600

    Updated wording of function comment

--- source/ProjectEulerProblemOne.swift
+++ source/ProjectEulerProblemOne.swift
@@ -1,6 +1,6 @@
 
 
-// Prject Euler Problem One
+// Problem one from Project Euler
 func sumMultiplesOfThreeAndFive(under upperBound: Int) -> Int {
     return (0..<upperBound).lazy.filter { $0.isMultiple(of: 3) || $0.isMultiple(of: 5) }.reduce(0, +)
 }
