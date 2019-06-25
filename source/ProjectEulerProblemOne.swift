ad9e32e3a36ceada402604d7785636a3b7277cff
Austin Van Alfen <austin.van.alfen@gmail.com>
2019-06-24 21:30:01 -0600

    Add files via upload
    First problem from Project Euler

---
+++ source/ProjectEulerProblemOne.swift
@@ -0,0 +1,8 @@
+
+
+// Prject Euler Problem One
+func sumMultiplesOfThreeAndFive(under upperBound: Int) -> Int {
+
+    return (0..<upperBound).lazy.filter { $0.isMultiple(of: 3) || $0.isMultiple(of: 5) }.reduce(0, +)
+
+}
