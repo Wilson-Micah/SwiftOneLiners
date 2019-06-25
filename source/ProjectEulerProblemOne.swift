4b4748a77fe01d92a64aa9630aa53b0c223b7cfa
Austin Van Alfen <austin.van.alfen@gmail.com>
2019-06-24 21:33:55 -0600

    Update ProjectEulerProblemOne.swift

--- source/ProjectEulerProblemOne.swift
+++ source/ProjectEulerProblemOne.swift
@@ -2,7 +2,5 @@
 
 // Prject Euler Problem One
 func sumMultiplesOfThreeAndFive(under upperBound: Int) -> Int {
-
     return (0..<upperBound).lazy.filter { $0.isMultiple(of: 3) || $0.isMultiple(of: 5) }.reduce(0, +)
-
 }
