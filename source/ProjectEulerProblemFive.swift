0c0266a56ef7ddf756ff5019787c788514c1e58a
Austin Van Alfen <austin.van.alfen@gmail.com>
2019-06-24 21:33:32 -0600

    Update ProjectEulerProblemFive.swift

--- source/ProjectEulerProblemFive.swift
+++ source/ProjectEulerProblemFive.swift
@@ -2,9 +2,7 @@
 
 // Problem 5 from Project Euler
 func smallestMultipleDivisibleByEveryNumber(under: Int) -> Int {
-
     return (1..<5000).first { currentNumber in !(1...under).lazy.map{ divisor in currentNumber.isMultiple(of: divisor) }.contains(false)}!
-
 }
 
 
