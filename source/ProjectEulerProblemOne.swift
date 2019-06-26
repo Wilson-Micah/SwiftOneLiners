1fe0760c50ac4c91383f615a9d2abd1919d6fedc
micahtwilson <micahw@sisna.com>
2019-06-25 18:40:03 -0600

    Fixed a typo

--- source/ProjectEulerProblemOne.swift
+++ source/ProjectEulerProblemOne.swift
@@ -5,4 +5,4 @@ func sumMultiplesOfThreeAndFive(under upperBound: Int) -> Int {
     return (0..<upperBound).lazy.filter { $0.isMultiple(of: 3) || $0.isMultiple(of: 5) }.reduce(0, +)
 }
 
-sumMultiplesOfThreeAndFive(under: 1000)
+print(sumMultiplesOfThreeAndFive(under: 1000))
