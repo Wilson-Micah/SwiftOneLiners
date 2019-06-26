5c24a8aa440f6bb6d010df75ccc7da7c0d461b89
Braden Scothern <BradenScothern@gmail.com>
2019-06-25 22:58:49 -0600

    Removed unneeded '-> Result' and types on closure parameters

--- source/GCDIterative.swift
+++ source/GCDIterative.swift
@@ -1,6 +1,6 @@
 // GCD - Euclidean Algorithm
 func gcd(_ a: Int, _ b: Int) -> Int {
-    return { (context: UnsafeMutablePointer<(Int, Int)>) -> Int in return { (value: (Int, Int), _) -> Int in value.0 }({ _ in  context.move() }({ while context.pointee.0 != context.pointee.1 { if context.pointee.0 > context.pointee.1 { context.pointee.0 -= context.pointee.1 } else { context.pointee.1 -= context.pointee.0 } } }()), context.deallocate()) }({ (context: UnsafeMutablePointer<(Int, Int)>) -> UnsafeMutablePointer<(Int, Int)> in return { _ in context }(context.initialize(to: (a, b))) }(UnsafeMutablePointer<(Int, Int)>.allocate(capacity: 1)))
+    return { context in return { value, _ in value.0 }({ _ in  context.move() }({ while context.pointee.0 != context.pointee.1 { if context.pointee.0 > context.pointee.1 { context.pointee.0 -= context.pointee.1 } else { context.pointee.1 -= context.pointee.0 } } }()), context.deallocate()) }({ context in return { _ in context }(context.initialize(to: (a, b))) }(UnsafeMutablePointer<(Int, Int)>.allocate(capacity: 1)))
 }
 
 print(gcd(45, 333)) // 9
